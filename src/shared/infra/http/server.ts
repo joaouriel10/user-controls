3c3d881ee9933a4819b25642c0e89b00c089bf67
joaouriel10 <joaouriel2016@outlook.com>
2021-03-08 21:23:34 -0300

    Primeiro commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
